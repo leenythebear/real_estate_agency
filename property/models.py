eaa21825b47a7e62703d2de3f4451ea291d6c05d
leeny_the_bear <leeny@inbox.ru>
2022-09-01 14:10:43 +0700

    del blank=True

--- property/models.py
+++ property/models.py
@@ -46,7 +46,7 @@ class Flat(models.Model):
         null=True,
         blank=True,
         db_index=True)
-    new_building = models.BooleanField('Новостройка', blank=True, null=True)
+    new_building = models.BooleanField('Новостройка', null=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
