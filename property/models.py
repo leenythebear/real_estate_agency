9d88ab28020b2351cd70abae535e38e9140d5f2f
leeny_the_bear <leeny@inbox.ru>
2022-09-02 17:35:18 +0700

    rename field

--- property/models.py
+++ property/models.py
@@ -66,7 +66,10 @@ class Complaint(models.Model):
 
 
 class Owner(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
+    owner_name = models.CharField('ФИО владельца', max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     owner_pure_phone = PhoneNumberField(blank=True, verbose_name='Нормализованный номер владельца')
     flats = models.ManyToManyField('Flat', verbose_name='Квартиры в собственности', related_name='owners')
+
+    def __str__(self):
+        return self.owner_name
