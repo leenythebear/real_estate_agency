ccb06d8722b2c03a29c2051533f3583302487cd8
leeny_the_bear <leeny@inbox.ru>
2022-09-08 15:47:07 +0700

    use decorator for register models

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class OwnerInstanceInline(admin.TabularInline):
     raw_id_fields = ['owner']
 
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address']
     readonly_fields = ['created_at']
@@ -20,15 +21,12 @@ class FlatAdmin(admin.ModelAdmin):
                ]
 
 
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ['user', 'flat']
 
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     list_display = ('name', 'pure_phonenumber')
     raw_id_fields = ('flats',)
-
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
